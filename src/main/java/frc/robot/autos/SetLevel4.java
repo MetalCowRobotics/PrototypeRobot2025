f81dbcdb7a13406cecffa79b8136fab3892fee3a
ABHI-5678 <vikhyath.c@metalcowrobotics.com>
2025-02-17 12:41:30 -0600

    Auto Commands

--- src/main/java/frc/robot/autos/SetLevel4.java
+++ src/main/java/frc/robot/autos/SetLevel4.java
@@ -3,14 +3,12 @@ package frc.robot.autos;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.ElevatorSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.NoteTransitSubsystem;
 
 /*This command toggles the shooter on/off */
-public class StopIntake extends Command{
+public class SetLevel4 extends Command{
     @Override
     public void execute(){
-        IntakeSubsystem.getInstance().stopIntake();
+        ElevatorSubsystem.getInstance().L4_Distance();
         SmartDashboard.putString("auto", "running");
     }
 }
